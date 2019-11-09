a37f1c15c7e32ac484145e732989d79361c57515
andrei_kuturmazov <andrei.kuturmazov@gmail.com>
2019-11-09 15:53:48 +0300

    First task

--- src/Task_one/TaskOne.java
+++ src/Task_one/TaskOne.java
@@ -11,41 +11,41 @@ import java.io.InputStreamReader;
 
 public class TaskOne {
 
-    private static String COUNTABLE = "четным";
-    private static String UNCOUNTABLE = "нечетным";
-    private static String SIMPLE = "простым";
-    private static String CONSTRUCT = "составным";
+    private static final String INPUTSTRING = "Введите, пожалуйста, число для проверки";
+    private static final String ERRORMESSAGE = "Вы ввели неверное значение";
+    private static final String ZEROINPUT = "Ноль не может участвовать в проверке";
+    private static final String COUNTABLE = "четным";
+    private static final String UNCOUNTABLE = "нечетным";
+    private static final String SIMPLE = "простым";
+    private static final String CONSTRUCT = "составным";
+    private static final String OUTPUTSTRING = "Введенное вами число %s является %s, %s числом";
 
-    public static void main(String[] args) {
-
-        System.out.println("Введите, пожалуйста, число для проверки");
+    public static void main(String[] args) throws IOException {
+        System.out.println(INPUTSTRING);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         try {
             int input = Integer.parseInt(reader.readLine());
-
             if (input == 0) {
-                System.out.print("Ноль не может участвовать в проверке. ");
-                throw new IOException();
+                System.out.println(ZEROINPUT);
             }
-
-            String output = String.format("Введенное вами число %s является %s, %s числом", input, isCountable(input), isConstruct(input));
+            String output = String.format(OUTPUTSTRING, input, isCountable(input), isConstruct(input));
             System.out.println(output);
-            reader.close();
+
         } catch (IOException e) {
-            System.err.println("Вы ввели неверное значение");
+            System.err.println(ERRORMESSAGE);
         }
+        reader.close();
     }
 
-    public static String isCountable(int a) {
-        return a % 2 == 0 ? COUNTABLE : UNCOUNTABLE;
+    public static String isCountable(int input) {
+        return input % 2 == 0 ? COUNTABLE : UNCOUNTABLE;
     }
 
-    public static String isConstruct(int a) {
-        int abs = Math.abs(a);
+    public static String isConstruct(int input) {
+        int absolutValue = Math.abs(input);
         String output = SIMPLE;
-        for (int i = 2; i < abs; i++) {
-            if (abs % i == 0) {
+        for (int index = 2; index < absolutValue; index++) {
+            if (absolutValue % index == 0) {
                 output = CONSTRUCT;
                 break;
             }
